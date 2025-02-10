603aa12e07efcf6497ec734638e05e9760770b76
jaagualimpia <jaagualimpia25@gmail.com>
2025-02-09 20:01:07 -0500

    Things with jenkinsfile again 3

--- src/app/(noauth)/api/test/route.ts
+++ src/app/(noauth)/api/test/route.ts
@@ -1,10 +1,9 @@
 import { NextResponse } from "next/server";
 
 export async function GET() {
-    const apiUrl = process.env["API_URL"] + "weight"
-    console.log(apiUrl)
-    let thing = await fetch(apiUrl, {method: "GET"})
-    const data = await thing.json()
+    // const apiUrl = process.env["API_URL"] + "weight"
+    // let thing = await fetch(apiUrl, {method: "GET"})
+    // const data = await thing.json()
     
     return NextResponse.json({hola: "string"})
 }
